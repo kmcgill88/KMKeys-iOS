c6801f1f04acec2b3536720d2a779b18bba65edf
Kevin McGill <kevin@mcgilldevtech.com>
2017-04-20 09:29:57 -0500

    I want to unit test but I suck at it in Swift

--- Example/Tests/KMKeysTest.swift
+++ Example/Tests/KMKeysTest.swift
@@ -6,55 +6,13 @@ import Nimble
 import KMKeys
 
 class KMKeysTest: QuickSpec {
+    
     override func spec() {
         describe("init KMKeys: default") {
-            
-            
-            let keys = KMKeys()
-            
-            
             it("has a toolbar and text field") {
-                //expect() == 2
+                //let keys = KMKeys()
+                //expect(keys.subviews.count) == 2
             }
-            
-
-//            it("can do maths") {
-//                expect(1) == 2
-//            }
-//
-//            it("can read") {
-//                expect("number") == "string"
-//            }
-//
-//            it("will eventually fail") {
-//                expect("time").toEventually( equal("done") )
-//            }
-            
-//            context("these will pass") {
-//
-//                it("can do maths") {
-//                    expect(23) == 23
-//                }
-//
-//                it("can read") {
-//                    expect("🐮") == "🐮"
-//                }
-//
-//                it("will eventually pass") {
-//                    var time = "passing"
-//
-//                    DispatchQueue.main.async {
-//                        time = "done"
-//                    }
-//
-//                    waitUntil { done in
-//                        Thread.sleep(forTimeInterval: 0.5)
-//                        expect(time) == "done"
-//
-//                        done()
-//                    }
-//                }
-//            }
         }
     }
 }
