d967ee3c7679fe55d31489e906e05930414a1ddb
Kevin McGill <kevin@mcgilldevtech.com>
2017-05-11 20:36:33 -0500

    Add ability to set font and color of placeholder.

--- Example/KMKeys/ViewController.swift
+++ Example/KMKeys/ViewController.swift
@@ -34,8 +34,8 @@ class ViewController: UIViewController {
         //
         keys.textField.textAlignment = .center
         keys.textField.placeholder = "Placeholder Text Here"
-//        keys.textField.font = UIFont(name: "", size: CGFloat.pi)
-//        keys.textField.placeholderColor = UIColor.init(white: 255/255, alpha: 0.75)
+        keys.textField.font = UIFont(name: "American Typewriter", size: 16.0)!
+        keys.textField.placeholderColor = UIColor.init(white: 255/255, alpha: 0.75)
         keys.textField.backgroundColor = .brown
         keys.textField.textColor = .white
         keys.textField.tintColor = .white
--- KMKeys/Classes/KMTextField.swift
+++ KMKeys/Classes/KMTextField.swift
@@ -9,20 +9,29 @@
 import UIKit
 
 open class KMTextField: UITextField {
-
+    
     open override var font: UIFont? {
         didSet {
-            
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder ?? "", attributes: getPlaceholderAttributes())
         }
     }
     
     open var placeholderColor: UIColor? {
-        get {
-            return self.placeholderColor
-        }
-        set {
-            self.attributedPlaceholder = NSAttributedString(string:self.placeholder ?? "", attributes:[NSForegroundColorAttributeName: newValue!])
+        didSet {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder ?? "", attributes: getPlaceholderAttributes())
         }
     }
-
+    
+    private func getPlaceholderAttributes() -> [String:Any] {
+        var attributes:[String : Any] = [:]
+        
+        if let color = self.placeholderColor {
+            attributes[NSForegroundColorAttributeName] = color
+        }
+        
+        if let validFont = self.font {
+            attributes[NSFontAttributeName] = validFont
+        }
+        return attributes
+    }
 }
